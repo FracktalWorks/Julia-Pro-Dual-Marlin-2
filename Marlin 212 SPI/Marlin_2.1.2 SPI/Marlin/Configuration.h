35f06ff19558c9de2b0adccfa37fe1e6f46cd852
nishbilu <nishbilu@gmail.com>
2023-04-17 14:57:24 +0530

    inverted y stepper enable logic

--- Marlin 212 SPI/Marlin_2.1.2 SPI/Marlin/Configuration.h
+++ Marlin 212 SPI/Marlin_2.1.2 SPI/Marlin/Configuration.h
@@ -1607,7 +1607,7 @@
 // For Inverting Stepper Enable Pins (Active Low) use 0, Non Inverting (Active High) use 1
 // :{ 0:'Low', 1:'High' }
 #define X_ENABLE_ON 0
-#define Y_ENABLE_ON 0
+#define Y_ENABLE_ON 1
 #define Z_ENABLE_ON 0
 #define E_ENABLE_ON 0 // For all extruders
 //#define I_ENABLE_ON 0
